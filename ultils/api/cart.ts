afec1ba3b8d722e3d1444dfc0ee2c0126b069ff2
Duc dung <tranthideptrai@gmail.com>
2024-09-26 23:41:27 +0700

    update history

--- ultils/api/cart.ts
+++ ultils/api/cart.ts
@@ -1,29 +1,43 @@
 import { apiCall } from "../func/api";
 
 interface CartItem {
-    productId: string;
-    productName: string;
-    quantity: number;
-    price: number;
-    productLink: string;
-    cashbackPercentage: number
+  productId: string;
+  productName: string;
+  quantity: number;
+  price: number;
+  productLink: string;
+  cashbackPercentage: number;
 }
 
 export interface GetCartResponse {
-    cartItems: CartItem[];
-    totalPrice: number;
+  cartItems: CartItem[];
+  total: number;
+  pag: number;
 }
 
-export const getCart = async (token: string, signal?: AbortSignal): Promise<GetCartResponse> => {
-    return apiCall<GetCartResponse>('/api/cart', 'GET', undefined, token, signal);
+export const getCart = async (
+  token: string,
+  page: number,
+  signal?: AbortSignal
+): Promise<GetCartResponse> => {
+  return apiCall<GetCartResponse>(
+    `/api/cart?pag=${page}`,
+    "GET",
+    undefined,
+    token,
+    signal
+  );
 };
 
 interface EditCartResponse {
-    success: boolean;
+  success: boolean;
 }
 
-export const editCart = async (productId: string, quantity: number, token: string): Promise<EditCartResponse> => {
-    const data = { productId, quantity };
-    return apiCall<EditCartResponse>('/api/cart', 'PUT', data, token);
+export const editCart = async (
+  productId: string,
+  quantity: number,
+  token: string
+): Promise<EditCartResponse> => {
+  const data = { productId, quantity };
+  return apiCall<EditCartResponse>("/api/cart", "PUT", data, token);
 };
-
