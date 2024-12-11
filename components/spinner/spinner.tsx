c64d833d0d3cf27821b7545465263bc278b58446
Duc dung <tranthideptrai@gmail.com>
2024-12-12 00:02:25 +0700

    add img avatar

--- components/spinner/spinner.tsx
+++ components/spinner/spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div role="status" className="flex justify-center w-full p-4">
+    <div role="status" className="flex justify-center w-full p-[120px]">
      <img src="/loading.gif" alt="loading" />
       <span className="sr-only">Loading...</span>
     </div>
